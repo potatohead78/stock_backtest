ddb18f130e173feeed4768a174cea64645f4f963
potatohead78 <132098359+potatohead78@users.noreply.github.com>
2023-05-19 15:40:34 +0900

    간단 수정

--- plotting.py
+++ plotting.py
@@ -28,11 +28,13 @@ class Plotting:
         fig.add_trace(buy,row=2,col=1)
         fig.add_trace(sell,row=2,col=1)
 
-        fig.update_layout(autosize=True, xaxis1_rangeslider_visible=False, xaxis2_rangeslider_visible=False, margin=dict(l=50,r=50,t=50,b=50), template='seaborn', title=f"{datetime.now().strftime('%Y.%m.%d.%H%M%S')}")
+        title = datetime.now().strftime('%Y.%m.%d.%H%M%S')
+
+        fig.update_layout(autosize=True, xaxis1_rangeslider_visible=False, xaxis2_rangeslider_visible=False, margin=dict(l=50,r=50,t=50,b=50), template='seaborn', title=f"{title}")
         fig.update_xaxes(tickformat='%y년%m월%d일', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=2, gridcolor='lightgray', showline=True,linewidth=2, linecolor='black', mirror=True)
         fig.update_yaxes(tickformat=',d', zeroline=True, zerolinewidth=1, zerolinecolor='black', showgrid=True, gridwidth=2, gridcolor='lightgray',showline=True,linewidth=2, linecolor='black', mirror=True)
         fig.update_traces(xhoverformat='%y년%m월%d일')
 
         config = dict({'scrollZoom': True})
-        po.write_html(fig, file='결과/.html')
+        po.write_html(fig, file=f"결과/{title}.html")
         fig.show(config=config)
